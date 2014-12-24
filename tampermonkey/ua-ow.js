59c5ed7f7a6007899c29f8185325f213fa14b544
Dante Sun <dante.sun@gmail.com>
2014-12-25 02:00:27 +0800

    User Agent for Outlook Web

---
+++ tampermonkey/ua-ow.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name       UA | Outlook Web
+// @namespace  https://mail.citrix.com
+// @version    0.1
+// @match       *://mail.citrix.com/*
+// @user-agent Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0;  rv:11.0) like Gecko
+// @copyright  2011+, You
+// ==/UserScript==
+~                                                                               
+~                                                                               
+~                                                                                                               
+~                                                                               
+~                                                                               
+~                                                                                                               
+~                                                                               
+~                                                                               
+~                                                                                                               
+~                                                                               
+~                                                                               
+~                                                                                                               
+~                                                                               
+~                                                                               
+~                                             
