ffc17d5f5e24a3a6880ca8a618acb48dbf225ffc
Dante Sun <dante.sun@gmail.com>
2015-01-14 11:42:53 +0800

    change to zprezto

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s extglob
 cd $HOME
-if ! [ -d .oh-my-zsh ]; then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#if ! [ -d .oh-my-zsh ]; then
+#  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#fi
+if ! [ -d .zprezto ]; then
+  git clone --recursive https://github.com/dantesun/prezto.git ".zprezto"
 fi
+for rcfile in "$HOME"/.zprezto/runcoms/!(README.md); do
+  dotfile="$HOME/.$(basename ${rcfile})"
+  if [ -e $dotfile ]; then
+    echo "[WARN] $dotfile exists, skipping $rcfile"
+    continue
+  fi
+  ln -sv $rcfile $dotfile
+done
 
-for file in vim bash_profile bashrc pentadactylrc dircolors gtkrc-2.0 gtkrc-solarized vimrc Xdefaults xpdfrc zshrc; do
+for file in vim bash_profile bashrc pentadactylrc dircolors gtkrc-2.0 gtkrc-solarized vimrc Xdefaults xpdfrc tmux.conf; do
   dotfile="$PWD/.$file"
   src="system/.$file"
   if [ -e "$dotfile" ]; then
